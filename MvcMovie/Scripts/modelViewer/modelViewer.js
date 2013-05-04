f00aa8d402360d8c39d0826262a2ebcccd4cf587
Eugen Dundukov <eugendun@gmail.com>
2013-05-04 21:27:50 +0200

    TrackballControl added.

--- MvcMovie/Scripts/modelViewer/modelViewer.js
+++ MvcMovie/Scripts/modelViewer/modelViewer.js
@@ -1,4 +1,5 @@
 ﻿/// <reference path="../three/three.js" />
+/// <reference path="../controls/TrackballControls.js" />
 
 var $canvas = $("#modelViewerWebGLCanvas");
 var params = { canvas: $canvas.get(0) };
@@ -13,15 +14,33 @@ var view_angle = 45,
 
 var renderer = new THREE.WebGLRenderer(params);
 var camera = new THREE.PerspectiveCamera(view_angle, aspect, near, far);
+camera.position.z = 5;
+renderer.setClearColor('black');
+
+
 var scene = new THREE.Scene();
 
-scene.add(camera);
-camera.position.z = 5;
+var controls = new THREE.TrackballControls(camera, $canvas.get(0));
+controls.rotateSpeed = 2.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+
+controls.noZoom = false;
+controls.noPan = false;
+
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
+
 renderer.setSize(width, height);
 
 var pointLight = new THREE.PointLight(0xFFFFFF);
 pointLight.position.set(10, 50, 130);
 scene.add(pointLight);
+
+var ambientLight = new THREE.AmbientLight(0x0A0A0A);
+scene.add(ambientLight);
+
+//var diffuseLight = new THREE.Lig
 
 var geometry = new THREE.CubeGeometry(2, 2, 2);
 geometry.dynamic = true;
@@ -29,8 +48,10 @@ var material = new THREE.MeshLambertMaterial({ color: 0xff0000 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+render();
+
 function render() {
     requestAnimationFrame(render);
+    controls.update();
     renderer.render(scene, camera);
 }
-render();
