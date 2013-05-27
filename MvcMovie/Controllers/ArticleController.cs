e496edf1a1470ba88a3b030d1b41c5ae324f7565
Eugen Dundukov <eugendun@gmail.com>
2013-05-28 00:53:31 +0200

    Now it is possible to create, edit and delete articles.

--- MvcMovie/Controllers/ArticleController.cs
+++ MvcMovie/Controllers/ArticleController.cs
@@ -17,11 +17,54 @@ namespace MvcMovie.Controllers
         }
 
         [HttpPost]
-        public string CreateArticle(FormCollection fc)
+        [ValidateInput(false)]
+        public ActionResult Create(FormCollection fc)
         {
+            if (fc.Count != 1 || !Request.IsAuthenticated)
+            {
+                return HttpNotFound();
+            }
 
+            var newArticle = new Article();
+            newArticle.Content = fc.GetValue(fc.GetKey(0)).AttemptedValue;
+            newArticle.Author = "Eugen Dundukov";
+            db.Articles.Add(newArticle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-            return "CreateArticle invoked!";
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(FormCollection fc)
+        {
+            if (!Request.IsAuthenticated || fc.Count != 2)
+            {
+                return HttpNotFound();
+            }
+
+            var content = fc.GetValues(0)[0];
+            var id = Convert.ToInt32(fc.GetValues(1)[0]);
+
+            var article = db.Articles.Find(id);
+            article.Content = content;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(FormCollection fc)
+        {
+            if (!Request.IsAuthenticated || fc.Count != 1)
+            {
+                return HttpNotFound();
+            }
+            var id = Convert.ToInt32(fc.GetValues(0)[0]);
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
