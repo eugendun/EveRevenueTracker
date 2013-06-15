1557877e68c8d6c77812e5caa5cfbefcb3eaed0b
Eugen Dundukov <eugendun@gmail.com>
2013-06-15 14:14:18 +0200

    In AccounController registration commented out. New SocialGameController for the SocialGame site. Unity web export for the SocialGame added.

--- MvcMovie/Controllers/AccountController.cs
+++ MvcMovie/Controllers/AccountController.cs
@@ -74,23 +74,24 @@ namespace MvcMovie.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterModel model)
         {
-            if (ModelState.IsValid)
-            {
-                // Attempt to register the user
-                try
-                {
-                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
-                    WebSecurity.Login(model.UserName, model.Password);
-                    return RedirectToAction("Index", "Home");
-                }
-                catch (MembershipCreateUserException e)
-                {
-                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
-                }
-            }
+            //if (ModelState.IsValid)
+            //{
+            //    // Attempt to register the user
+            //    try
+            //    {
+            //        WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+            //        WebSecurity.Login(model.UserName, model.Password);
+            //        return RedirectToAction("Index", "Home");
+            //    }
+            //    catch (MembershipCreateUserException e)
+            //    {
+            //        ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
+            //    }
+            //}
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            //// If we got this far, something failed, redisplay form
+            //return View(model);
+            return HttpNotFound("Neue Registrierungen werden nicht akzeptiert!");
         }
 
         //
