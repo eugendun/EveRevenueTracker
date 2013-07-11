ebb35be5b20fb3a61af4706615b4f49379b4b3d0
Eugen Dundukov <eugendun@gmail.com>
2013-07-11 11:43:03 +0200

    EveApi and new EveApiControllers added. ReactionTestController added, with some basic google chart tool diagrams. Packages updated.

---
+++ MvcMovie/Controllers/EveApi.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class EveApi
+    {
+        public string getServerStatus()
+        {
+            string url = "https://api.eveonline.com/server/ServerStatus.xml.aspx";
+            return getDataFromUrl(url);
+        }
+
+        public string getCharacters()
+        {
+            string url = "https://api.eveonline.com/account/Characters.xml.aspx";
+            List<string> parameters = new List<string>();
+            parameters.Add("keyID=999390");
+            parameters.Add("vCode=ox1bvi5104jSN5vEtYO3GxkhDtmYfKm2LufHah7jp4kYgrs41t8FIzhoEKcqstiy");
+            url = attachParamsToUrl(url, parameters);
+            return getDataFromUrl(url);
+        }
+
+        public string getWalletTransactions()
+        {
+            string url = "https://api.eveonline.com/char/WalletTransactions.xml.aspx";
+            List<string> parameters = new List<string>();
+            parameters.Add("keyID=999390");
+            parameters.Add("vCode=ox1bvi5104jSN5vEtYO3GxkhDtmYfKm2LufHah7jp4kYgrs41t8FIzhoEKcqstiy");
+            parameters.Add("characterID=...");
+            url = attachParamsToUrl(url, parameters);
+            return getDataFromUrl(url);
+        }
+
+        public string getWalletJournal()
+        {
+            string url = "https://api.eveonline.com/char/WalletJournal.xml.aspx";
+            List<string> parameters = new List<string>();
+            parameters.Add("keyID=999390");
+            parameters.Add("vCode=ox1bvi5104jSN5vEtYO3GxkhDtmYfKm2LufHah7jp4kYgrs41t8FIzhoEKcqstiy");
+            parameters.Add("characterID=...");
+            url = attachParamsToUrl(url, parameters);
+            return getDataFromUrl(url);
+        }
+
+        private string getDataFromUrl(string url)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                Byte[] pageData = client.DownloadData(url);
+                string pageXml = Encoding.ASCII.GetString(pageData);
+                return pageXml;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("EVE-API Failure: \n" + e.Message);
+            }
+        }
+
+        private string attachParamsToUrl(string url, List<string> parameters)
+        {
+            url += "?";
+            if (parameters.Count > 0)
+                url += parameters[0];
+            for (int i = 1; i < parameters.Count; i++)
+            {
+                url += "&" + parameters[i];
+            }
+
+            return url;
+        }
+    }
+}
---
+++ MvcMovie/Controllers/EveApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace MvcMovie.Controllers
+{
+    public class EveApiController : Controller
+    {
+        EveApi eveApi = new EveApi();
+
+        //
+        // GET: /EveApi/
+
+        public ActionResult Index()
+        {
+            string xmlData = eveApi.getServerStatus();
+
+
+
+            //XmlDocument doc = new XmlDocument();
+            //doc.LoadXml(xmlData);
+
+            //foreach (XmlNode row in doc.SelectNodes("//row"))
+            //{
+            //    string name = row.Attributes["name"].Value;
+
+            //}
+
+            ContentResult res = new ContentResult();
+            res.Content = xmlData;
+
+            //return View();
+            return res;
+        }
+    }
+}
