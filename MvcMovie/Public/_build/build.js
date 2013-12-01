56ecfd73ddb3ae66610325a3871efc004042044d
Eugen Dundukov <eugendun@gmail.com>
2013-12-01 09:52:02 +0100

    build file template for requirejs added used on build progress by node.exe

---
+++ MvcMovie/Public/_build/build.js
@@ -0,0 +1,24 @@
+﻿({
+    appDir: '../js',
+    baseUrl: './',
+    mainConfigFile: '../js/config.js',
+    dir: '../release',
+    optimize: 'uglify2',
+    uglify2: {
+        output: {
+            beautify: false
+        }
+    },
+    modules: [
+                { name: 'views/EveApi/Index'},
+            ],
+    onBuildRead: function(moduleName, path, contents) {
+        if (moduleName === "config") {
+            return contents.replace("/Public/js", "/Public/release");
+        }
+        return contents;
+    },
+    // to exclude all text! files set to false
+    inlineText: true
+})
+
